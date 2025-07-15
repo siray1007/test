f09f8eaea5b3c2015c7ec32672a56baa0de15b06
SIRAY <1791863775@qq.com>
2025-07-15 16:50:27 +0800

    hallo world

--- flask1.py
+++ flask1.py
@@ -17,8 +17,11 @@ def home():
     age = request.args.get("age")
     pwd = request.args.get("pwd")
     print(age,pwd)
-
     return"失败"
 
+@app.route("/Hallo world")
+def Hallo_world():
+    return"Hallo world"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host = "127.0.0.1",port = 1145)
