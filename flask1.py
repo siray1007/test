500cc0f3a5f0e922abcf4b8b5f67b0b52dda4553
SIRAY <1791863775@qq.com>
2025-07-15 15:27:07 +0800

    第二次

--- flask1.py
+++ flask1.py
@@ -1,20 +1,24 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
-#当http://127.0.0.1:5000/index时执行index
+#http://127.0.0.1:5000/index->执行index
+##http://127.0.0.1:5000/index?age=20&pwd=123>执行index并接收20和123（传参）
 @app.route("/index")
 def index():
+    age = request.args.get("age")
+    pwd = request.args.get("pwd")
+    print(age,pwd)
     return"成功"
 
-
-
-#当http://127.0.0.1:5000/home时执行home
+#http://127.0.0.1:5000/home->执行home
 @app.route("/home")
 def home():
+    age = request.args.get("age")
+    pwd = request.args.get("pwd")
+    print(age,pwd)
+
     return"失败"
 
-
-
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",post=5000)
+    app.run()
