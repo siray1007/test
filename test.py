542ced71bba39a916b26de678c9b59900e3a0b2d
SIRAY <1791863775@qq.com>
2025-07-16 16:10:54 +0800

    test

---
+++ test.py
@@ -0,0 +1,44 @@
+# year = 2025
+# event = 'Referendum'
+# print(f'Results of the {year} {event}')
+
+# import math
+# print(f'The value of pi is approximately {math.pi:.5f}.')
+
+# table = {'Sjoerd': 412711.85676575, 'Jack': 40981, 'Dcab': 7678}
+# for name, phone in table.items():
+# print(f'{name:10} ==> {phone:<10}')
+
+# print('We are the {} who say "{}!"'.format('knights', 'Ni'))
+
+# print('{0} and {1} or {2}'.format('spam', 'eggs', 'x'))
+# print('{1} and {2} or {0}'.format('spam', 'eggs', 'x'))
+
+# print('This {food} is {adjective}.'.format(food='spam', adjective='absolutely horrible'))
+
+# table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 8637678}
+# print('Jack: {0[Jack]:d};'' Sjoerd: {0[Sjoerd]:d}; ''Dcab: {0[Dcab]:d}'.format(table))
+
+# def scope_test():
+#     def do_local():
+#         spam = "local spam"
+
+#     def do_nonlocal():
+#         nonlocal spam
+#         spam = "nonlocal spam"
+
+#     def do_global():
+#         global spam
+#         spam = "global spam"
+
+#     spam = "test spam"
+#     do_local()
+#     print("After local assignment:", spam)
+#     do_nonlocal()
+#     print("After nonlocal assignment:", spam)
+#     do_global()
+#     print("After global assignment:", spam)
+
+
+# scope_test()
+# print("In global scope:", spam)
